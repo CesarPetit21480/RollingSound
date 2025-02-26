36d4133e15ebccb02d305d80c1d87f29bb4eae82
JoaquinPC <joaquinpavon8@gmail.com>
2025-02-26 13:19:06 -0300

    Guardando cambios de administracion antes del pull

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ function App() {
               element={
                 <ProtectedRoute>
                   <Administration />
+             
                 </ProtectedRoute>
               }
             />
