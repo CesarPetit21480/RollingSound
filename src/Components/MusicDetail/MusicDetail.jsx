988555e84975efe5acc05d9f586189c3d1927fb5
cesar <cesar_petit@hotmail.com>
2025-02-27 18:37:36 -0300

    Merge con main

--- src/Components/MusicDetail/MusicDetail.jsx
+++ src/Components/MusicDetail/MusicDetail.jsx
@@ -1,37 +1,8 @@
-import './MusicDetail.css'
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  CardTitle,
-  CardText,
-} from "react-bootstrap";
-function MusicDetail({musica}) {
 
+
+const MusicDetail = () => {
   return (
-    <div className='cardDetailContainer'>
-       <div className='row'>
-        {
-          musica.map((tema)=>(
-            
-            <div key={tema.id}>
-
-
-              
-            </div>
-          ))
-
-
-        }
-
-
-
-
-       </div>
-
-
-
-    </div>
+    <div>MusicDetail</div>
   )
 }
 
