37de41e0c77031f6947d4593d883f3f4693c0406
MaritoSebastian <maritosebastianchavez1975@gmai.com>
2025-03-05 21:11:42 -0300

    se realiza los comentarios en el codigo y se cambio el carrousel para que sea automatico

--- src/Components/MusicDetail/MusicDetail.jsx
+++ src/Components/MusicDetail/MusicDetail.jsx
@@ -1,31 +1,25 @@
 import { Modal, Button } from "react-bootstrap";
 import "./MusicDetail.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function MusicDetail({ musica, onClose, isCollapsed }) {
-  console.log("Datos de musica:", musica);
+  console.log("Datos de musica:", musica.imagenesCarrusel);
   if (!musica) return null;
+  //
   const [index, setIndex] = useState(0);
-  const nextSlide = () => {
-  setIndex((prev) => {
-    const newIndex = (prev + 1) % musica.imagenesCarrusel.length;
-    console.log('Índice después de siguiente:', newIndex); // Muestra el nuevo índice
-    return newIndex;
-  });
-};
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => {
+        //hace que el cuando llegue a 3 vuelva a cero ya que cuando llega  a 3 lo divide en el length que es 3 ,el resto es ceero y retona eso 
+        const newindex = (prev + 1) % musica.imagenesCarrusel.length;
+        return newindex;
+      });
+    }, 3000);
 
-const prevSlide = () => {
-  setIndex((prev) => {
-    const newIndex = (prev - 1 + musica.imagenesCarrusel.length) % musica.imagenesCarrusel.length;
-    console.log('Índice después de anterior:', newIndex); // Muestra el nuevo índice
-    return newIndex;
-  });
-};
-
-  
-  
-  
-
+    //limpiaos e intervalo
+    return () => clearInterval(interval);
+  }, [musica.imagenesCarrusel.length]);
+ 
   return (
     <Modal
       show={true}
@@ -64,66 +58,42 @@ const prevSlide = () => {
                 transition: "transform 0.5s ease-in-out",
               }}
             >
-              { musica.imagenesCarrusel && musica.imagenesCarrusel.length >0?( musica.imagenesCarrusel.map((img,index) => (
-                
-                <div
-                  key={index}
-                  className="carrousel-item"
-                  style={{ flex: "0 0 100%" }}
-                >
+              {/* en esta lline se donde se hace el mapeo  hace una comprobaacion si erray no tiene nada devuelve la imagen de portada si no devuelve el carrousel*/ }
+              {musica.imagenesCarrusel && musica.imagenesCarrusel.length > 0 ? (
+                musica.imagenesCarrusel.map((img, index) => (
+                  <div
+                    key={index}
+                    className="carrousel-item"
+                    style={{ flex: "0 0 100%" }}
+                  >
+                    <img
+                      src={img}
+                      alt={musica.titulo}
+                      className="carrousel-item rounded img-fluid"
+                      style={{
+                        width: "100%",
+                        height: "200px",
+                        objectFit: "cover",
+                        aspectRatio: "16/9",
+                      }}
+                    />
+                  </div>
+                ))
+              ) : (
+                <div>
                   <img
-                
-                    src={`${img}?t=${new Date().getTime()}`} 
-                    alt={img}
-                    className="carrousel-item rounded img-fluid"
+                    src={musica.linkImagen}
+                    alt={musica.titulo}
+                    className="rounded img-fluid"
                     style={{
                       width: "100%",
-                      height: "200px",
+                      height: "180px",
                       objectFit: "cover",
-                      aspectRatio:"16/9"
                     }}
                   />
                 </div>
-              ))):(
-
-                <div>
-    <img
-      src={musica.linkImagen}
-      alt={musica.titulo}
-      className="rounded img-fluid"
-      style={{ width: "100%", height: "180px", objectFit: "cover" }}
-    />
-  </div>
-              
-            )}
+              )}
             </div>
-            <button
-              onClick={prevSlide}
-              className="carousel-button prev text-bg-success text-dark fw-bold"
-              style={{
-                position: "absolute",
-                top: "50%",
-                left: "10px",
-                transform: "translateY(-50%)",
-                zIndex: 1,
-
-              }}
-            >
-              &lt;
-            </button>
-            <button
-              onClick={nextSlide}
-              className="carousel-button next text-bg-success text-dark fw-bold"
-              style={{
-                position: "absolute",
-                top: "50%",
-                right: "10px",
-                transform: "translateY(-50%)",
-                zIndex: 1,
-              }}
-            >
-              &gt;
-            </button>
           </div>
 
           <div style={{ flex: 1 }}>
@@ -157,7 +127,9 @@ const prevSlide = () => {
 
 export default MusicDetail;
 
-  /* <Swiper
+/*aqui fui poniendo los codigo que fui usando y descartando por que no logre que funcionen los carousel de las primeras 8 imagenes
+lo dejo por las dudas cuando revisemos el codigo se necesite usar*/
+/* <Swiper
             modules={[Navigation, Pagination]}
             navigation
             pagination={{ clickable: true }}
@@ -183,3 +155,49 @@ export default MusicDetail;
             })}
           </Swiper>*/
 
+/*const nextSlide = () => {
+    setIndex((prev) => {
+      const newIndex = (prev + 1) % musica.imagenesCarrusel.length;
+      console.log("Índice después de siguiente:", newIndex); // Muestra el nuevo índice
+      return newIndex;
+    });
+  };
+
+  const prevSlide = () => {
+    setIndex((prev) => {
+      const newIndex =
+        (prev - 1 + musica.imagenesCarrusel.length) %
+        musica.imagenesCarrusel.length;
+      console.log("Índice después de anterior:", newIndex); // Muestra el nuevo índice
+      return newIndex;
+    });
+  };*/
+/*<button
+              onClick={prevSlide}
+              className="carousel-button prev  text-dark fw-bold"
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "10px",
+                transform: "translateY(-50%)",
+                
+                zIndex: 1,
+                fontSize:"60px"
+              }}
+            >
+              &lt;
+            </button>
+            <button
+              onClick={nextSlide}
+              className="carousel-button next  fw-bold"
+              style={{
+                position: "absolute",
+                top: "50%",
+                right: "10px",
+                transform: "translateY(-50%)",
+                zIndex: 1,
+                fontSize:"60px"
+              }}
+            >
+              &gt;
+            </button>*/
