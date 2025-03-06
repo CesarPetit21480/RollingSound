c603c640358c50a728af34f00041bc3b20842b03
Petit Martin Cesar Gaston <cpetit@inter.corp>
2025-03-06 11:07:21 -0300

    deploy

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -20,16 +20,4 @@ function NotFoundPage() {
     </div>
   );
 }
-
 export default NotFoundPage;
-
-/* 
-const NotFoundPage = () => {
-  return (
-    <div className="mt-5 pt-5 pb-5 d-flex justify-content-center">
-      NotFoundPage
-    </div>
-  )
-}
-
-export default NotFoundPage */
